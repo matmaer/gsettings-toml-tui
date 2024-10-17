e281b3647595e005174db0dee75a8bd79a00a81a
mm <mm@sc1a.local>
2024-10-17 18:24:28 +0200

    add config.py module with functions to read the toml and tcss file, also adding tcss file

---
+++ src/tuiapp/config.py
@@ -0,0 +1,15 @@
+import tomllib
+from pathlib import Path
+
+
+CONFIGS_DIR = Path(__file__).resolve().parent / "configs"
+
+TUI_TCSS = CONFIGS_DIR / "tui.tcss"
+
+
+def get_toml():
+    try:
+        with open(CONFIGS_DIR / "gsettings.toml", "rb") as f:
+            return tomllib.load(f)
+    except FileNotFoundError as not_found:
+        raise not_found
