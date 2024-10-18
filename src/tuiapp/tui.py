f5a9326330ee673383172bcfb0ee31301b5795e7
mm <mm@sc1a.local>
2024-10-18 07:13:47 +0200

    added new bash_run method that uses subprocess to execute the command, and sends the curated output to the yielded RichLog generator.
    Removed useless code from the rlog method.

--- src/tuiapp/tui.py
+++ src/tuiapp/tui.py
@@ -1,3 +1,5 @@
+import subprocess
+
 from textual import on
 from textual.app import App, ComposeResult
 from textual.containers import Horizontal, Vertical
@@ -20,9 +22,41 @@ class Atui(App):
     ]
     CSS_PATH = TUI_TCSS
 
-    def rlog(self, to_print) -> None:
-        rlog = self.query_one(RichLog)
-        rlog.write(to_print)
+    def rlog(self, to_print: str) -> None:
+        self.query_one(RichLog).write(to_print)
+
+    def bash_run(self, commands: list) -> None:
+        for command in commands:
+            try:
+                pretty_cmd = f"[bold]$_ [/][cyan]{command}[/]"
+                result = subprocess.run(
+                    command,
+                    capture_output=True,
+                    check=True,
+                    encoding="utf-8",
+                    shell=True,
+                    timeout=2,
+                )
+                pretty_exit = f"([green]exit code {result.returncode}[/])"
+                if result.returncode == 0 and result.stdout == "":
+                    self.rlog(f"{pretty_cmd} [green](success)[/]")
+                elif result.stdout != "":
+                    self.rlog(f"{pretty_cmd} {pretty_exit}")
+                    self.rlog(result.stdout)
+                else:
+                    to_print = f"{pretty_cmd} [red]{pretty_exit}[/]"
+                    self.rlog(f"[yellow]{to_print}[/]")
+                    if result.stdout != "":
+                        self.rlog(result.stdout)
+                    if result.stderr != "":
+                        self.rlog(result.stderr)
+            except subprocess.CalledProcessError as e:
+                if e.stdout != "":
+                    self.rlog(e.stdout)
+                elif e.stderr != "":
+                    self.rlog(e.stderr)
+                else:
+                    self.rlog(e)
 
     def compose(self) -> ComposeResult:
         with Horizontal():
@@ -43,7 +77,7 @@ class Atui(App):
 
     @on(Button.Pressed, "#show_current")
     def show_current_settings(self):
-        self.rlog("Current settings")
+        self.bash_run(["gsettings list-recursively"])
 
     @on(Button.Pressed, "#clear_richlog")
     def clear_richlog(self):
