9d3ccc634c18ad6cc3a59f3ab19655e6e3d6ebbc
mm <mm@sc1a.local>
2024-10-18 03:23:52 +0200

    - updated tcss code for all elements - added button to display contents of the toml file

--- src/tuiapp/tui.py
+++ src/tuiapp/tui.py
@@ -1,16 +1,15 @@
 from textual import on
 from textual.app import App, ComposeResult
 from textual.containers import Horizontal, Vertical
-
 from textual.widgets import Button, Footer, RichLog
 
-from .config import TUI_TCSS
+from .config import TUI_TCSS, get_toml
 
 
 class Sidebar(Vertical):
     def compose(self) -> ComposeResult:
-        yield Button(label="SHOW ENVIRONMENT", id="show_env")
-        yield Button(label="UPDATE SETTINGS", id="update_gsettings")
+        yield Button(label="DESIRED SETTINGS", id="show_toml")
+        yield Button(label="CURRENT SETTINGS", id="show_current", disabled=True)
         yield Button(label="CLEAR RICHLOG", id="clear_richlog")
 
 
@@ -34,9 +33,9 @@ class Atui(App):
     def action_toggle_sidebar(self):
         self.query_one(Sidebar).toggle_class("-hidden")
 
-    @on(Button.Pressed, "#show_env")
-    def show_environment(self):
-        self.query_one(RichLog).write("environment info here")
+    @on(Button.Pressed, "#show_toml")
+    def show_current_settings(self):
+        self.query_one(RichLog).write(get_toml())
 
     @on(Button.Pressed, "#clear_richlog")
     def clear_richlog(self):
