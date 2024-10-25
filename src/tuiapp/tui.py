8843038fcde543ae81e3d3d2725e99aff8064256
mm <mm@sc1a.local>
2024-10-25 22:01:39 +0200

    working button to pretty print current config

--- src/tuiapp/tui.py
+++ src/tuiapp/tui.py
@@ -1,24 +1,29 @@
+import pickle
 import subprocess
+from pathlib import Path
 
 from textual import on
 from textual.app import App, ComposeResult
 from textual.containers import Horizontal, Vertical
 from textual.widgets import Button, Footer, Header, RichLog
 
-from .config import TUI_TCSS, get_toml
+# import tomllib
+
+
+CONFIGS = Path(__file__).resolve().parent / "configs"
+TCSS_PATH = CONFIGS / "tui.tcss"
 
 
 class Sidebar(Vertical):
     def compose(self) -> ComposeResult:
         yield Button(
-            label="Generate",
-            id="gen_toml",
-            tooltip="Generate toml file with current settings",
+            label="All Settings (long time to load first time)",
+            id="all_settings",
+            tooltip="show or load all current settings",
         )
         yield Button(
-            label="Customize",
-            id="custom_toml",
-            tooltip="create or show customized settings toml file",
+            label="Custom Settings",
+            id="custom_settings",
         )
         yield Button(
             label="Compare",
@@ -28,128 +33,171 @@ class Sidebar(Vertical):
         yield Button(
             label="Update",
             id="update_settings",
-            tooltip="Update settings with 'gsettings set' to match the custom toml file",
+            tooltip="Update settings with 'gsettings set' command",
         )
         yield Button(label="Clear \nRichLog", id="clear_richlog")
 
 
 class GSettings(App):
+    def __init__(self):
+        super().__init__()
+        # self.custom_settings_file = CONFIGS / "custom_settings.toml"
+        self.all_settings_pickle = CONFIGS / "all_settings.pickle"
+        if self.all_settings_pickle.exists():
+            with open(self.all_settings_pickle, "rb") as pickle_file:
+                self.all_settings = pickle.load(pickle_file)
+        else:
+            self.all_settings = {}
+
     BINDINGS = [
         ("s", "toggle_sidebar", "Toggle Maximized"),
         ("q", "quit", "Quit"),
     ]
-    CSS_PATH = TUI_TCSS
+    CSS_PATH = TCSS_PATH
 
-    def __init__(self):
-        super().__init__()
-        self.desired = get_toml()
-        # list of dicts with schema, key, old value and new value
-        self.updates = []
-
-    def rlog(self, to_print: str) -> None:
+    def rlog(self, to_print: str, highlight=True) -> None:
+        rich_log = self.query_one(RichLog)
+        if not highlight:
+            rich_log.highlight = False
+        else:
+            rich_log.highlight = True
         self.query_one(RichLog).write(to_print)
 
-    def run_gsettings_command(self, command: str) -> str:
+    def run_command(self, command: list) -> str:
         try:
             result = subprocess.run(
                 command,
                 capture_output=True,
+                # check=True will raise an exception if return code is not 0
                 check=True,
                 encoding="utf-8",
-                shell=True,
                 timeout=1,
             )
-            if result.returncode == 0:
-                if result.stdout == "":
-                    # gsettings set returns nothing on success
-                    if "gsettings set" in command:
-                        return "success"
-                    if "gsettings list-schemas" in command:
-                        return result.stdout
-                    if "gsettings list-keys" in command:
-                        return ["no_keys"]
-            else:
-                return "no_stdout"
-            return result.stderr
+            # always remove leading and trailing spaces, tabs and newlines
+            # \' will make sure the result is not returned with both single
+            # and double quotes
+            result.stdout = result.stdout.strip('\t\n" ')
+            return result
         except subprocess.CalledProcessError as e:
-            if e.stdout != "":
-                self.rlog(e.stdout)
-            if e.stderr != "":
-                self.rlog(e.stderr)
-            else:
-                self.rlog(e)
+            message = f"'{e.cmd}' failed, exit conde {e.returncode}"
+            self.rlog(f"[tomato bold]{message}[/]")
+            self.rlog(f"[lightpink bold]'{e.stdout}'[/]")
             return "error"
 
-    def create_current_settings_toml(self) -> None:
-        schemas = self.run_gsettings_command("gsettings list-schemas")
-        return schemas
+    def list_schemas(self) -> list:
+        result = self.run_command(["gsettings", "list-schemas"])
+        if result.stdout != "" and result != "error":
+            schemas = result.stdout.split("\n")
+            for schema in schemas:
+                command = ["gsettings", "list-children", schema]
+                children = self.run_command(command)
+                if children != "":
+                    # the schema has children, no keys for settings
+                    schemas.remove(schema)
+            return schemas
+        # return empty list if no schemas can be retrieved
+        return []
 
-    def get_current_value(self, schema: str, schema_key: str) -> str:
-        gsettings_cmd = f"gsettings get {schema} {schema_key}"
-        current_value = self.run_gsettings_command(gsettings_cmd)
-        # trim from both ends: single quotes, newlines and spaces
-        current_value = current_value.strip("'\n ")
-        # 'gsettings get' sometimes returns types, sometimes not.
-        # So, remove types from the output to compare values.
-        # to check if ok for 'gsettings set' command and all cases
-        to_remove = ["uint32 ", "uint64 ", "int32 ", "int64 "]
-        for text in to_remove:
-            current_value = current_value.replace(text, "")
-        return current_value
+    def list_keys(self, schema: str) -> list:
+        result = self.run_command(["gsettings", "list-keys", schema])
+        if result.stdout in ["error", ""]:
+            return []
+        all_keys = result.stdout.split("\n")
+        keys = all_keys.copy()
+        for key in all_keys:
+            to_exclude = [
+                # "window-",
+                "recent-",
+                "selected-",
+                "recently-",
+                "recent-",
+                "last-",
+            ]
+            for pattern in to_exclude:
+                if pattern in key:
+                    keys.remove(key)
+                    break
+        return keys
 
-    def update_settings(self) -> None:
-        self.rlog("[cyan underline]Settings Update[/]")
-        # self.rlog("No changes to update, run 'Compare Settings' first")
-        if len(self.updates) == 0:
-            self.rlog("Nothing to do, run 'Compare Settings' to check.")
+    def get_value(self, schema: str, key: str) -> str:
+        result = self.run_command(["gsettings", "get", schema, key])
+        if result != "error":
+            return result.stdout
+        # return empty string if no value can be retrieved
+        return ""
+
+    def get_type_and_range(self, schema: str, key: str) -> str:
+        result = self.run_command(["gsettings", "range", schema, key])
+        if result.stdout != "error" and result.stdout != "":
+            # if ["error", ""] in result.stdout:
+            if "\n" in result.stdout:
+                type_range_str = result.stdout.replace("\n", ", ")
+            else:
+                type_range_str = result.stdout
+            # make the type more readable
+            types_replacements = {
+                "enum ": "type: enumeration; ",
+                "flags ": "type: lags; ",
+                "range d ": "type; range of double precision floating point; ",
+                "range i ": "type: range of integers; ",
+                "range ": "type: range; ",
+                "type a ": "type: array; ",
+                "type ai ": "type: array of integers; ",
+                "type as ": "type: array of strings; ",
+                "type b ": "type: boolean; ",
+                "type d ": "type: double precision floating point; ",
+                "type i ": "type: integer; ",
+                "type s ": "type: string",
+                "type t ": "type: t; ",
+                "type u ": "type: unsigned integer; ",
+                "type av ": "type: array of variants; ",
+            }
+            # construct the range and type string
+            for current, new in types_replacements.items():
+                if current in type_range_str:
+                    type_range_str = type_range_str.replace(current, new)
+            return str(type_range_str)
+        # return empty string if no type and range can be retrieved
+        return ""
+
+    def load_all_settings(self) -> dict:
+        # create nested dict with all settings
+        settings_dict = {}
+        schemas = self.list_schemas()
+        for schema in schemas:
+            keys = self.list_keys(schema)
+            if keys == [] or keys == "" or keys == "error":
+                continue
+            settings_dict[schema] = {}
+            for key in keys:
+                settings_dict[schema][key] = {}
+                current = self.get_value(schema, str(key))
+                type_range_str = self.get_type_and_range(schema, key)
+                # construct the value range
+                values_dict = {
+                    "value": current,
+                    "type_range": str(type_range_str),
+                }
+                settings_dict[schema][key] = values_dict
+        with open(self.all_settings_pickle, "wb") as f:
+            pickle.dump(settings_dict, f)
+        return settings_dict
+
+    def write_all_settings(self) -> None:
+        # yield LoadingIndicator("loading all settings...") todo
+        self.rlog("[cyan underline]All Settings[/]")
+        if not self.all_settings_pickle.exists():
+            self.all_settings = self.load_all_settings()
         else:
-            for update in self.updates:
-                schema = update["schema"]
-                key = update["key"]
-                old_value = update["old_value"]
-                new_value = update["new_value"]
-                command = f'gsettings set {schema} {key} "{new_value}"'
-                result = self.run_gsettings_command(command)
-                if result == "success":
-                    to_print = [
-                        f"  [yellow]{schema}[/] [green bold]UPDATED[/]",
-                        f"    [green]{key}: [/]{new_value} (old value: {old_value})",
-                    ]
-                    self.rlog("\n".join(to_print))
-                else:
-                    self.rlog(f"Error running '{command}'")
-            self.updates = []
-        self.rlog("")
-
-    def compare_settings(self) -> list:
-        self.rlog("[cyan underline]Settings Comparison[/]")
-        for schema, keys in self.desired.items():
-            changes_needed_for_schema = False
-            # self.rlog(f"[yellow]{schema}[/]")
-            for key, des_val in keys.items():
-                cur_val = self.get_current_value(schema, key)
-                if cur_val == "error":
-                    self.rlog(f"Error getting {schema} {key} value, skipping")
-                else:
-                    if str(des_val) != str(cur_val):
-                        to_print = [
-                            f"[yellow]{schema}[/][red] TO UPDATE[/]",
-                            f"  [green]{key}:[/] {cur_val}",
-                            f"    [bold]desired: {des_val}[/]",
-                        ]
-                        self.rlog("\n".join(to_print))
-                        self.updates += [
-                            {
-                                "schema": schema,
-                                "key": key,
-                                "old_value": cur_val,
-                                "new_value": des_val,
-                            }
-                        ]
-                        changes_needed_for_schema = True
-            if not changes_needed_for_schema:
-                self.rlog(f"[yellow]{schema}[/]: [green]no changes needed[/]")
-        self.rlog("")
+            with open(self.all_settings_pickle, "rb") as pickle_file:
+                self.all_settings = pickle.load(pickle_file)
+        for schema, keys in self.all_settings.items():
+            self.rlog(f"[yellow]{schema}[/]")
+            for key, value in keys.items():
+                self.rlog(f"  [green]{key}[/]: [bold]{value['value']}[/]")
+                type_str = self.all_settings[schema][key]["type_range"]
+                type_message = f"[#87CEFA]{type_str}[/]"
+                self.rlog(f"    {type_message}", highlight=False)
 
     def compose(self) -> ComposeResult:
         yield Header()
@@ -162,31 +210,32 @@ class GSettings(App):
             )
         yield Footer()
 
-    def action_toggle_sidebar(self):
-        self.query_one(Sidebar).toggle_class("-hidden")
+    @on(Button.Pressed, "#all_settings")
+    def load_all_settings_button(self):
+        self.write_all_settings()
 
-    @on(Button.Pressed, "#gen_toml")
-    def generate_toml_settings(self):
-        self.rlog("[cyan underline]Generate Toml file: to do[/]")
-        self.rlog("")
-
-    @on(Button.Pressed, "#custom_toml")
-    def show_toml_button(self):
-        self.rlog("[cyan underline]Desired Settings[/]")
-        self.rlog(self.desired)
-        self.rlog("")
+    @on(Button.Pressed, "#custom_settings")
+    def show_custom(self):
+        self.rlog("[cyan underline]Custom Settings[/]")
+        self.rlog("[lightblue]to be implemented[/]")
 
     @on(Button.Pressed, "#compare_settings")
     def compare_settings_button(self):
-        self.compare_settings()
+        self.rlog("[cyan underline]Settings Comparison[/]")
+        self.rlog("[lightblue]to be implemented[/]")
 
     @on(Button.Pressed, "#update_settings")
     def update_settings_button(self):
-        self.update_settings()
+        self.rlog("[cyan underline]Settings Update[/]")
+        self.rlog("[lightblue]to be implemented[/]")
 
     @on(Button.Pressed, "#clear_richlog")
     def clear_richlog(self):
         self.query_one(RichLog).clear()
+        self.rlog("[cyan]RichLog Cleared[/]")
+
+    def action_toggle_sidebar(self):
+        self.query_one(Sidebar).toggle_class("-hidden")
 
 
 def run_tui():
